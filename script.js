7e10aab31ae4adda606bf9b374e772aa59257acd
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2023-08-14 21:59:17 +0545

    Update script.js
    Add functions to the new buttons added

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
-var humanPoints = 0;
-var computerPoints = 0;
-function getComputerChoice(choice){
-    const choices = ["rock", "paper", "scissor"];
-    var number = Math.floor(Math.random() * 3);
-    
-    document.getElementById("output-id").innerHTML = choices[number];;
-    
+let loopCount = 0;
+let playerScore =0;
+let computerScore = 0;
 
-    if((choice === "paper") & (choices[number] === "rock") ){
-        humanPoints += 1;
-        console.log(humanPoints + "humanPoints");
-        document.getElementById("human-id").innerHTML = humanPoints;
+function yourChoice(choice){
+    if(playerScore === 5){
+        window.alert("You have won!")
+    }
+    else if(computerScore === 5){
+        window.alert("Computer Won!")
+    }
+    else{
+        winner(choice,getComputerChoice());
     }
 
-    else if((choice === "paper") & (choices[number] === "scissor") ){
-        computerPoints += 1;
-        console.log(computerPoints + "computerpoints");
-        document.getElementById("computer-id").innerHTML = computerPoints;
-    }
-};
+}
 
+function getComputerChoice(){
+    const choice = ["Rock", "Paper", "Scissor"];
+
+    let randChoice = Math.floor(Math.random() * 3);
+
+    return choice[randChoice];
+}
+
+function winner(playerSelection, computerSelection){
+    if((playerSelection === "Rock") & (computerSelection === "Paper")){
+        computerScore += 1;
+        document.getElementById("computerScoreId").innerHTML = computerScore;
+        return "You lose Paper beats Rock.";
+    }
+    else if((playerSelection === "Rock") & (computerSelection === "Scissor")){
+        playerScore += 1;
+        document.getElementById("humanScoreId").innerHTML = playerScore;
+        return "You win Rock beats Scissor.";
+    }
+    else if((playerSelection === "Rock") & (computerSelection === "Rock")){
+        return "You tied";
+    }
+    else if((playerSelection === "Paper") & (computerSelection === "Rock")){
+        playerScore += 1;
+        document.getElementById("humanScoreId").innerHTML = playerScore;
+        return "You win Paper beats Rock";
+    }
+    else if((playerSelection === "Paper") & (computerSelection === "Scissor")){
+        computerScore += 1;
+        document.getElementById("computerScoreId").innerHTML = computerScore;
+        return "You lose Scissor beats Paper";
+    }
+    else if((playerSelection === "Paper") & (computerSelection === "Paper")){
+        return "You tied";
+    }
+    else if((playerSelection === "Scissor") & (computerSelection === "Paper")){
+        
+        playerScore += 1;
+        document.getElementById("humanScoreId").innerHTML = playerScore;
+        return "You win Scissor beats Paper.";
+    }
+    else if((playerSelection === "Scissor") & (computerSelection === "Rock")){
+
+        computerScore += 1;
+        document.getElementById("computerScoreId").innerHTML = computerScore;
+        return "You lose Rock beats Scissor";
+    }
+    else if((playerSelection === "Scissor") & (computerSelection === "Scissor")){
+        return "You tied.";
+    }
+}

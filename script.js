34a6c88378db54a1f96a774f812908c9d3894d44
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2023-08-16 21:10:30 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,11 @@
-let loopCount = 0;
-let playerScore =0;
-let computerScore = 0;
-
-function yourChoice(choice){
-    
-    winner(choice,getComputerChoice());
-    if(playerScore === 5){
-        window.alert("You have won!")
+function createGrid(numGrids){
+    for(var rows = 0; rows < numGrids; rows++){
+        for(var columns = 0; columns < numGrids; columns++){
+            document.getElementById("container").innerHTML += "<div class='grid' id='grid'></div>";
+        }
     }
-    else if(computerScore === 5){
-        window.alert("Computer Won!")
-    }
+    $(".grid").width(`${960/numGrids}px`);
+    $(".grid").height(`${960/numGrids}px`);
 }
 
-function getComputerChoice(){
-    const choice = ["Rock", "Paper", "Scissor"];
-
-    let randChoice = Math.floor(Math.random() * 3);
-
-    return choice[randChoice];
-}
-
-function winner(playerSelection, computerSelection){
-    if((playerSelection === "Rock") & (computerSelection === "Paper")){
-        document.getElementById("plrImg").src = "images/rock.jpg";
-        document.getElementById("comImg").src = "images/paper.jpg";
-
-        document.getElementById("winner").innerHTML = "Computer won!";
-        computerScore += 1;
-        document.getElementById("computerScoreId").innerHTML = computerScore;
-    }
-    else if((playerSelection === "Rock") & (computerSelection === "Scissor")){
-        playerScore += 1;
-        document.getElementById("plrImg").src = "images/rock.jpg";
-        document.getElementById("comImg").src = "images/scissor.jpg";
-        document.getElementById("humanScoreId").innerHTML = playerScore;
-
-        document.getElementById("winner").innerHTML = "Player won!";
-    }
-    else if((playerSelection === "Rock") & (computerSelection === "Rock")){
-        document.getElementById("plrImg").src = "images/rock.jpg";
-        document.getElementById("comImg").src = "images/rock.jpg";
-
-        document.getElementById("winner").innerHTML = "Tied!";
-    }
-    else if((playerSelection === "Paper") & (computerSelection === "Rock")){
-        playerScore += 1;
-        document.getElementById("plrImg").src = "images/paper.jpg";
-        document.getElementById("comImg").src = "images/rock.jpg";
-        document.getElementById("humanScoreId").innerHTML = playerScore;
-        document.getElementById("winner").innerHTML = "Player won!";
-
-    }
-    else if((playerSelection === "Paper") & (computerSelection === "Scissor")){
-        computerScore += 1;
-        document.getElementById("plrImg").src = "images/paper.jpg";
-        document.getElementById("comImg").src = "images/scissor.jpg";
-        document.getElementById("computerScoreId").innerHTML = computerScore;
-
-        document.getElementById("winner").innerHTML = "Computer won!";
-    }
-    else if((playerSelection === "Paper") & (computerSelection === "Paper")){
-        document.getElementById("plrImg").src = "images/paper.jpg";
-        document.getElementById("comImg").src = "images/paper.jpg";
-        document.getElementById("winner").innerHTML = "Tied!";
-    }
-    else if((playerSelection === "Scissor") & (computerSelection === "Paper")){
-        playerScore += 1;
-        document.getElementById("plrImg").src = "images/scissor.jpg";
-        document.getElementById("comImg").src = "images/paper.jpg";
-        document.getElementById("humanScoreId").innerHTML = playerScore;
-        document.getElementById("winner").innerHTML = "Player won!";
-    }
-    else if((playerSelection === "Scissor") & (computerSelection === "Rock")){
-        document.getElementById("plrImg").src = "images/scissor.jpg";
-        document.getElementById("comImg").src = "images/rock.jpg";
-        computerScore += 1;
-        document.getElementById("computerScoreId").innerHTML = computerScore;
-        document.getElementById("winner").innerHTML = "Computer won!";
-    }
-    else if((playerSelection === "Scissor") & (computerSelection === "Scissor")){
-        document.getElementById("plrImg").src = "images/scissor.jpg";
-        document.getElementById("comImg").src = "images/scissor.jpg";
-        document.getElementById("winner").innerHTML = "Tied!";
-    }
-}
+createGrid(16);

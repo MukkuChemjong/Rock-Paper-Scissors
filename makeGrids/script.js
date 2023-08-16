48c886aa9de08abc23dfcaf26d79892b7cd62106
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2023-08-16 22:47:17 +0545

    update index,script and styles
    Update the logic for how the grid is created and how it works

--- makeGrids/script.js
+++ makeGrids/script.js
@@ -4,8 +4,29 @@ function createGrid(numGrids){
             document.getElementById("container").innerHTML += "<div class='grid' id='grid'></div>";
         }
     }
+    
     $(".grid").width(`${960/numGrids}px`);
     $(".grid").height(`${960/numGrids}px`);
 }
-
 createGrid(16);
+
+function newGrid(){
+    document.getElementById("container").innerHTML = "";
+    let numGrids = window.prompt("Enter num of Grids:");
+    createGrid(numGrids);
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+    });
+}
+
+$(document).ready(function () {
+    createGrid(16);
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+    });
+});
+
+
+
+

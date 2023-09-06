b88635f58450c8db4ab31da77230cd5d5f952f6d
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2023-09-05 23:11:19 -0400

    Remove player 2 and add an "Ai"
    Remove player2 and add a function that randomly chooses a number between 1-9 and use that to play for the "ai".

--- Tictactoe/script.js
+++ Tictactoe/script.js
@@ -1,4 +1,3 @@
-let player1 = true;
 let firstHorizontalWin1 = 0;
 let secondHorizontalWin1 = 0;
 let thirdHorizontalWin1 = 0;
@@ -24,187 +23,254 @@ let thirdVerticalWin2 = 0;
 let firstDiagonalWin2 = 0;
 let secondDiagonalWin2 = 0;
 
+let squareChoices = [1,2,3,4,5,6,7,8,9];
+
+function generateRandomNumber(){
+    console.log(squareChoices);
+   
+    return squareChoices[Math.floor((Math.random() * squareChoices.length))];
+}
+
+function removeChoice(number){
+    const index = squareChoices.indexOf(number);
+
+    squareChoices.splice(index, 1);
+}
+
+function computerTurn(number){
+    if(number === 1){
+        firstHorizontalWin2 += 1;
+        firstDiagonalWin2 += 1;
+        firstVerticalWin2 += 1;
+        removeChoice(number);
+        if(firstHorizontalWin2 === 3 || firstDiagonalWin2 === 3 || firstVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+    
+    if(number === 2){
+        firstHorizontalWin2 += 1;
+        secondVerticalWin2 += 1;
+        removeChoice(number);
+        if(firstHorizontalWin2 === 3 || secondVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+        
+    }
+
+    if(number === 3){
+        firstHorizontalWin2 += 1;
+        secondDiagonalWin2 += 1;
+        thirdVerticalWin2 += 1;
+        removeChoice(number);
+        if(firstHorizontalWin2 === 3 || secondDiagonalWin2 === 3 || thirdVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+    if(number === 4){
+        secondHorizontalWin2 += 1;
+        firstVerticalWin2 += 1;
+        removeChoice(number);
+        if(secondHorizontalWin2 === 3 || firstVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+    if(number === 5){
+        secondHorizontalWin2 += 1;
+        secondVerticalWin2 += 1;
+        firstDiagonalWin2 += 1;
+        secondDiagonalWin2 += 1;
+        removeChoice(number);
+        if(secondHorizontalWin2 === 3 || secondVerticalWin2 === 3 || firstDiagonalWin2 === 3 || secondDiagonalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+
+    if(number === 6){
+        secondHorizontalWin2 += 1;
+        thirdVerticalWin2 += 1;
+        removeChoice(number);
+        if(secondHorizontalWin2 === 3 || thirdVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+
+    if(number === 7){
+        thirdHorizontalWin2 += 1;
+        firstVerticalWin2 += 1;
+        secondDiagonalWin2 += 1; 
+        removeChoice(number);
+        if(thirdHorizontalWin2 === 3 || firstVerticalWin2 === 3 || secondDiagonalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+    if(number === 8){
+        thirdHorizontalWin2 += 1;
+        secondVerticalWin2 += 1;
+        removeChoice(number);
+        if(thirdHorizontalWin2 === 3 || secondVerticalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+        
+
+    if(number === 9){
+        thirdHorizontalWin2 += 1;
+        thirdVerticalWin2 += 1;
+        firstDiagonalWin2 += 1;
+        removeChoice(number);
+        if(thirdHorizontalWin2 === 3 || thirdVerticalWin2 === 3 || firstDiagonalWin2 === 3){
+            alert("player 2 wins");
+        }
+    }
+
+    document.getElementById(`squareId${number}`).innerHTML = "o";
+    document.getElementById(`squareId${number}`).style.color = "black";
+    document.getElementById(`squareId${number}`).disabled;
+}
 
 function addSymbol(number){
     if(number === 1){
-        console.log(player1);
-        if(player1 === true){
-            firstHorizontalWin1 += 1;
-            firstDiagonalWin1 += 1;
-            firstVerticalWin1 += 1;
-            if(firstHorizontalWin1 === 3 || firstDiagonalWin1 === 3 || firstVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            firstHorizontalWin2 += 1;
-            firstDiagonalWin2 += 1;
-            firstVerticalWin2 += 1;
-            if(firstHorizontalWin2 === 3 || firstDiagonalWin2 === 3 || firstVerticalWin2 === 3){
-                alert("player 1 wins");
-            }
+        firstHorizontalWin1 += 1;
+        firstDiagonalWin1 += 1;
+        firstVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+        console.log(squareChoices);
+
+        if(firstHorizontalWin1 === 3 || firstDiagonalWin1 === 3 || firstVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
 
     if(number === 2){
-        if(player1 === true){
-            firstHorizontalWin1 += 1;
-            secondVerticalWin1 += 1;
-            if(firstHorizontalWin1 === 3 || secondVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            firstHorizontalWin2 += 1;
-            secondVerticalWin2 += 1;
-            if(firstHorizontalWin2 === 3 || secondVerticalWin2 === 3){
-                alert("player 2 wins");
-            }
+        firstHorizontalWin1 += 1;
+        secondVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(firstHorizontalWin1 === 3 || secondVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
 
     if(number === 3){
-        if(player1 === true){
-            firstHorizontalWin1 += 1;
-            secondDiagonalWin1 += 1;
-            thirdVerticalWin1 += 1;
-            if(firstHorizontalWin1 === 3 || secondDiagonalWin1 === 3 || thirdVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            firstHorizontalWin2 += 1;
-            secondDiagonalWin2 += 1;
-            thirdVerticalWin2 += 1;
-            if(firstHorizontalWin2 === 3 || secondDiagonalWin2 === 3 || thirdVerticalWin2 === 3){
-                alert("player 2 wins");
-            }
+        
+        firstHorizontalWin1 += 1;
+        secondDiagonalWin1 += 1;
+        thirdVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(firstHorizontalWin1 === 3 || secondDiagonalWin1 === 3 || thirdVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
 
     if(number === 4){
-        if(player1 === true){
-            secondHorizontalWin1 += 1;
-            firstVerticalWin1 += 1;
-            if(secondHorizontalWin1 === 3 || firstVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            secondHorizontalWin2 += 1;
-            firstVerticalWin2 += 1;
-            if(secondHorizontalWin2 === 3 || firstVerticalWin2 === 3){
-                alert("player 2 wins");
-            }
+       
+        secondHorizontalWin1 += 1;
+        firstVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(secondHorizontalWin1 === 3 || firstVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
     if(number === 5){
-        if(player1 === true){
-            secondHorizontalWin1 += 1;
-            secondVerticalWin1 += 1;
-            firstDiagonalWin1 += 1;
-            secondDiagonalWin1 += 1;
-            if(secondHorizontalWin1 === 3 || secondVerticalWin1 === 3 || firstDiagonalWin1 === 3 || secondDiagonalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            secondHorizontalWin2 += 1;
-            secondVerticalWin2 += 1;
-            firstDiagonalWin2 += 1;
-            secondDiagonalWin2 += 1;
-            if(secondHorizontalWin2 === 3 || secondVerticalWin2 === 3 || firstDiagonalWin2 === 3 || secondDiagonalWin2 === 3){
-                alert("player 2 wins");
-            }
+        
+        secondHorizontalWin1 += 1;
+        secondVerticalWin1 += 1;
+        firstDiagonalWin1 += 1;
+        secondDiagonalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(secondHorizontalWin1 === 3 || secondVerticalWin1 === 3 || firstDiagonalWin1 === 3 || secondDiagonalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
     if(number === 6){
-        if(player1 === true){
-            secondHorizontalWin1 += 1;
-            thirdVerticalWin1 += 1;
-            if(secondHorizontalWin1 === 3 || thirdVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            secondHorizontalWin2 += 1;
-            thirdVerticalWin2 += 1;
-            if(secondHorizontalWin2 === 3 || thirdVerticalWin2 === 3){
-                alert("player 2 wins");
-            }
+       
+        secondHorizontalWin1 += 1;
+        thirdVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(secondHorizontalWin1 === 3 || thirdVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
     if(number === 7){
-        if(player1 === true){
-            thirdHorizontalWin1 += 1;
-            firstVerticalWin1 += 1;
-            secondDiagonalWin1 += 1; 
-            if(thirdHorizontalWin1 === 3 || firstVerticalWin1 === 3 || secondDiagonalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            thirdHorizontalWin2 += 1;
-            firstVerticalWin2 += 1;
-            secondDiagonalWin2 += 1; 
-            if(thirdHorizontalWin2 === 3 || firstVerticalWin2 === 3 || secondDiagonalWin2 === 3){
-                alert("player 2 wins");
-            }
+       
+        thirdHorizontalWin1 += 1;
+        firstVerticalWin1 += 1;
+        secondDiagonalWin1 += 1; 
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(thirdHorizontalWin1 === 3 || firstVerticalWin1 === 3 || secondDiagonalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
     if(number === 8){
-        if(player1 === true){
-            thirdHorizontalWin1 += 1;
-            secondVerticalWin1 += 1;
-            if(thirdHorizontalWin1 === 3 || secondVerticalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            thirdHorizontalWin2 += 1;
-            secondVerticalWin2 += 1;
-            if(thirdHorizontalWin2 === 3 || secondVerticalWin2 === 3){
-                alert("player 2 wins");
-            }
+        
+        thirdHorizontalWin1 += 1;
+        secondVerticalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(thirdHorizontalWin1 === 3 || secondVerticalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
     if(number === 9){
-        if(player1 === true){
-            thirdHorizontalWin1 += 1;
-            thirdVerticalWin1 += 1;
-            firstDiagonalWin1 += 1;
-            if(thirdHorizontalWin1 === 3 || thirdVerticalWin1 === 3 || firstDiagonalWin1 === 3){
-                alert("player 1 wins");
-            }
-        }
-        else{
-            thirdHorizontalWin2 += 1;
-            thirdVerticalWin2 += 1;
-            firstDiagonalWin2 += 1;
-            if(thirdHorizontalWin2 === 3 || thirdVerticalWin2 === 3 || firstDiagonalWin2 === 3){
-                alert("player 2 wins");
-            }
+        thirdHorizontalWin1 += 1;
+        thirdVerticalWin1 += 1;
+        firstDiagonalWin1 += 1;
+        const index = squareChoices.indexOf(number);
+
+        squareChoices.splice(index, 1);
+
+        if(thirdHorizontalWin1 === 3 || thirdVerticalWin1 === 3 || firstDiagonalWin1 === 3){
+            alert("player 1 wins");
+            return;
         }
     }
 
-    if(player1 === true){
-        document.getElementById(`squareId${number}`).innerHTML = "x";
-        document.getElementById(`squareId${number}`).style.color = "black";
-        document.getElementById(`squareId${number}`).disabled;
-        player1 = false;
-    }
-    else{
-        document.getElementById(`squareId${number}`).innerHTML = "o";
-        document.getElementById(`squareId${number}`).style.color = "black";
-        document.getElementById(`squareId${number}`).disabled;
-        player1 = true;
-    }
+    document.getElementById(`squareId${number}`).innerHTML = "x";
+    document.getElementById(`squareId${number}`).style.color = "black";
+    document.getElementById(`squareId${number}`).disabled;
+
+    computerTurn(generateRandomNumber());
 }

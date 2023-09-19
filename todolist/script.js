f50afc66afc9531dafa9db3fac741448a93e1f7a
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2023-09-19 01:18:02 -0400

    Add folders object
    Add folders object that stores the tasks of the user

--- todolist/script.js
+++ todolist/script.js
@@ -1,10 +1,23 @@
+const folders = {};
+let currentFolder = "test";
+folders["test"] = [];
+
+
 function addFolder(){
     let folderName = document.querySelector(".nameInput");
     let folderStorage = document.querySelector(".folderStorage");
     let folder = document.createElement("button");
+    let tasks = document.querySelector(".tasks");
+
+    folder.setAttribute("class", "folderItem");
+    folder.setAttribute("onclick", `showTasks(${folderName.value})`);
 
     folder.innerHTML = folderName.value;
     folderStorage.appendChild(folder);
+    folders[`${currentFolder}`] = [];
+    currentFolder = folderName.value;
+    tasks.innerHTML = "";
+    console.log(folders);
 }
 
 function addTask(){
@@ -23,7 +36,11 @@ function addTask(){
         taskHolder.innerHTML += taskName.value + ("&nbsp;").repeat(10);
         taskHolder.innerHTML += date.value + ("&nbsp;".repeat(10));
         taskHolder.appendChild(finished);
+
+        folders[currentFolder].push(taskHolder);
+
         tasks.appendChild(taskHolder);
+
         document.querySelector(".taskName").value = "Your Task";
         document.querySelector(".date").value = "";
     };
@@ -31,4 +48,13 @@ function addTask(){
 
 function finished(div){
     div.remove();
+}
+
+function showTasks(){
+    let tasks = document.querySelector(".tasks");
+    tasks.innerHTML = "";
+
+    for(let i = 0; folders[currentFolder].length < 0;i++){
+        tasks.appendChild(folders[currentFolder][i]);
+    }
 }
